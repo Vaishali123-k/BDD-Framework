51670a600cdd8fceed44f82a2c5caf3cdd885636
Vaishali <Vaishali@LAPTOP-R3ON7IAM>
2025-04-22 22:25:31 +0530

    BDD Framework is created

---
+++ src/main/java/BDDFramework12/BDDFramework12/App.java
@@ -0,0 +1,13 @@
+package BDDFramework12.BDDFramework12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
